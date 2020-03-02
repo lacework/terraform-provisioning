8e3180452305aa5cffc266cb2111f4e556692e09
Mobeen Tariq <mobeen.tariq@lacework.net>
2020-03-02 12:06:28 -0800

    Initial commit for gcp terraform template
    Added output file and modified variables
    initial azure template commit
    updated azure template with comments from PR
    Created one main.tf for both config and audit log
    
    Signed-off-by: Mobeen Tariq <mobeen.tariq@lacework.net>

---
+++ gcp/output.tf
@@ -0,0 +1,12 @@
+output "privateKey" {
+  value = base64decode(google_service_account_key.service-account-key-lacework.private_key)
+}
+
+output "subscription" {
+  value = "projects/test-project-mobeen/subscriptions/${google_pubsub_subscription.lacework_subscription[0].name}"
+  depends_on = [google_pubsub_subscription.lacework_subscription]
+}
+
+output "existing_subscription" {
+  value = var.existing_bucket_name != "" ? "Use existing subscription associated with the existing bucket" : "Use subscription printed with output"
+}
---
+++ gcp/variables.tf
@@ -0,0 +1,48 @@
+variable "prefix" {
+  type = string
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "credentials_file" {
+  type = string
+  description = "Location of the credentials file for gcp"
+}
+
+variable "org_integration" {
+  type = bool
+  description = "If set to true, configure an organization level integration"
+  default = false
+}
+
+variable "project_id" {
+  type = string
+  description = "Id of the project"
+}
+
+variable "organization_id" {
+  type = string
+  description = "Id of the organization"
+}
+
+variable "region" {
+  type = string
+  description = "Region you want to create resources in"
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  description = "Zone you want to create resources in"
+  default = "us-central1-c"
+}
+
+variable "audit_log" {
+  type = bool
+  description = "If set to true, create resources for both Config and Audit Logs"
+}
+
+variable "existing_bucket_name" {
+  type = string
+  description = "Name of an existing bucket you want to send the logs to"
+  default = ""
+}
