1acd6893bfdf018f2ff475cf756601c8bebe1515
Salim Afiune <afiune@lacework.net>
2021-01-12 17:01:16 +0100

    feat(gcp): upgrade terraform inside shell_startup.sh (#107)
    * feat: upgrade terraform inside shell_startup.sh
    
    * feat: verify version of the Lacework CLI

--- gcp/shell_startup.sh
+++ gcp/shell_startup.sh
@@ -9,12 +9,16 @@ mkdir -p $HOME/bin
 
 # Install the Lacework CLI if it is not installed
 if [ ! -f "$HOME/bin/lacework" ]; then
+    echo "-> Installing the Lacework CLI..."
     curl https://raw.githubusercontent.com/lacework/go-sdk/master/cli/install.sh | bash -s -- -d $HOME/bin
+else
+    echo "-> The Lacework CLI is already installed!"
+    lacework version
 fi
 
 # Install Terraform if it is not installed
 if [ ! -f "$HOME/bin/terraform" ]; then
-    curl https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip --output $HOME/bin/terraform.zip
+    curl https://releases.hashicorp.com/terraform/0.14.4/terraform_0.14.4_linux_amd64.zip --output $HOME/bin/terraform.zip
     unzip $HOME/bin/terraform.zip -d $HOME/bin
     rm $HOME/bin/terraform.zip
 fi
